e0b8699823cc01eff7e57c01bfe6a1bc266696b8
CHENG ZENG <zengc@rose-hulman.edu>
2015-03-27 16:44:25 -0400

    Pass test for task 5

--- src/primegenerator/PrimeGenerator.java
+++ src/primegenerator/PrimeGenerator.java
@@ -7,6 +7,21 @@ public class PrimeGenerator {
 
 	public List<Integer> generate(int input) {
 		List<Integer> primes = new ArrayList<Integer>();
+		for (int i = 2; i < input; i++) {
+			if (isPrime(i)) {
+				primes.add(i);
+			}
+		}
 		return primes;
 	}
+
+	private boolean isPrime(int n) {
+		for (int i = 2; i < n; i++) {
+			if (n % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
 }
