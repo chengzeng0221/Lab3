910b6dd8ce58deaafbd5a03edb9b5cca1310ed2b
CHENG ZENG <zengc@rose-hulman.edu>
2015-03-27 14:31:06 -0400

    Test one
    Test that the generated list for 1 returns empty

---
+++ src/primefactorization/PrimeFactorization.java
@@ -0,0 +1,12 @@
+package primefactorization;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class PrimeFactorization {
+
+	public static List<Integer> generate(int i) {
+		return Arrays.asList();
+	}
+
+}
--- src/primefactorization/PrimeFactorizationTests.java
+++ src/primefactorization/PrimeFactorizationTests.java
@@ -2,13 +2,20 @@ package primefactorization;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Test;
 
 public class PrimeFactorizationTests {
-
+	
+	private List<Integer> list() {
+		return Arrays.asList();
+	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test_that_the_generated_list_for_1_returns_empty() {
+		assertEquals(list(), PrimeFactorization.generate(1));
 	}
 
 }
